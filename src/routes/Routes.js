dbdf3b7f54dbc8af4f497f209e1ac0de00914bc8
wahid30 <md.wahidulalamjp2@gmail.com>
2022-10-22 18:58:35 +0600

    compon and route added

---
+++ src/routes/Routes.js
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../components/Home/Home";
+import Main from "../layouts/Main";
+
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/home",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
