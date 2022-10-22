3b528c3b06b83d6093e2facf528c390ed2139ef3
wahid30 <md.wahidulalamjp2@gmail.com>
2022-10-22 22:42:45 +0600

    firebase congfig added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBUYGMQtYF4s-SkNokehS28Ts5eJ4GCzLs",
+  authDomain: "travel-monster-a058b.firebaseapp.com",
+  projectId: "travel-monster-a058b",
+  storageBucket: "travel-monster-a058b.appspot.com",
+  messagingSenderId: "891004624145",
+  appId: "1:891004624145:web:95b847feaf0be4ee9700b0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
